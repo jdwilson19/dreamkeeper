a932bc658d70d4f29b7722be695e3ffcb2b3677c
jdwilson19 <jdwilson8@dons.usfca.edu>
2025-05-06 07:22:39 -0700

    new update
    final

--- js/sketch2.js
+++ js/sketch2.js
@@ -1,35 +1,47 @@
+let img;
+let link;
+
 function windowResized(){
-    resizeCanvas(windowWidth, windowHeight)
+  resizeCanvas(windowWidth, windowHeight)
 }
 
 function preload() {
-    // Load image before setup
-    img = loadImage("blue_cloth.png");  // Replace with your image path
-  }
+  img = loadImage("cloud-cloth.png");
+}
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background('white');
-    let link = createA("http://127.0.0.1:5500/page5.html", "cloud"); 
-    link.position(745, 630);
-    link.style('font-family', 'Junge');
-  link.style('font-size', '50px');
+  createCanvas(windowWidth, windowHeight);
+  background('white');
+
+  link = createA("page5.html", "cloud"); 
+  link.style('font-family', 'Italianno');
+  link.style('font-size', '75px');
   link.style('color', '#FFD039');
   link.style('text-decoration', 'none');
 }
 
 function draw() { 
-    fill('black');
-    textFont("Junge");
-    textSize(50);
-    textAlign(CENTER); // Align text to the center
+  fill('black');
+  textFont("Junge");
+  textSize(50);
+  textAlign(CENTER);
 
-    text('That I may wrap them', width / 2, height / 6.5); // Top text
+  text('That I may wrap them', width / 2, height / 6.5); 
 
-    let imgHeight = img.height * 1; // Scale image height
-  let imgWidth = img.width * 1;   // Scale image width
+  let imgHeight = img.height * 1; 
+  let imgWidth = img.width * 1;  
   image(img, width / 2 - imgWidth / 2, height / 2 - imgHeight / 2, imgWidth, imgHeight); 
 
-    text('In a blue            -cloth', width / 2, height / 1.1); 
-}
+  let sentenceY = height / 1.1;
+  textAlign(LEFT);
 
+  let sentenceStartX = width / 2 - textWidth("In a blue cloud-cloth") / 2;
+  text("In a blue", sentenceStartX, sentenceY);
+
+  let cloudX = sentenceStartX + textWidth("In a blue ");
+  if (link) {
+    link.position(cloudX, sentenceY - 60);
+  }
+
+  text("-cloth", cloudX + textWidth("cloud "), sentenceY);
+}
--- js/sketch3.js
+++ js/sketch3.js
@@ -1,50 +1,85 @@
 let imgCloth, imgHand1, imgHand2;
+let handYStart;
+let handYTarget;
+let handSpeed = 0.1;
+let link;
 
-function windowResized(){
-    resizeCanvas(windowWidth, windowHeight)
-  }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
-  function preload() {
-    // Load image before setup
-    imgCloth = loadImage("blue_cloth.png");  // Image 1
-  imgHand1 = loadImage("hand1.png");       // Image 2
-  imgHand2 = loadImage("hand2.png");   
-  }
+function preload() {
+  imgCloth = loadImage("cloud-cloth.png");
+  imgHand1 = loadImage("hand1.png");
+  imgHand2 = loadImage("hand2.png");
+}
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background('white');
-    let link = createA("http://127.0.0.1:5500/page6.html", "world"); 
-    link.position(772, 608);
-    link.style('font-family', 'Junge');
+  createCanvas(windowWidth, windowHeight);
+  background('white');
+
+  handYStart = 0;
+  handYTarget = 0;
+
+  link = createA("page6.html", "world");
+  link.style('font-family', 'Junge');
   link.style('font-size', '50px');
   link.style('color', '#FFD039');
   link.style('text-decoration', 'none');
 }
 
-function draw() { 
-    fill('black');
-    textFont("Junge");
-    textSize(50);
-    textAlign(CENTER);  // Center the text
+//hand animation assisted by chatgbt
+function draw() {
+  background('white');
+  fill('black');
+  textFont("Junge");
+  textSize(50);
+  textAlign(CENTER);
 
-    let clothScale = 0.6;
-    let clothW = imgCloth.width * clothScale;
-    let clothH = imgCloth.height * clothScale;
-    image(imgCloth, width / 2 - clothW / 2, height / 2 - clothH / 2, clothW, clothH); 
-  
-    // Draw hand images *after* the cloth so they appear on top
-    let handScale = 0.4;
-    let handW = imgHand1.width * handScale;
-    let handH = imgHand1.height * handScale;
-  
-    // You can adjust Y to position them slightly above the cloth
-    let handY = height / 2 - clothH / 2 - handH / 1.5;
-  
-    image(imgHand1, width / 2 - handW * 1.2, handY, handW, handH); // Left hand
-    image(imgHand2, width / 2 + handW * 0.2, handY, handW, handH); // Right hand
-  
+  let clothScale = 0.6;
+  let clothW = imgCloth.width * clothScale;
+  let clothH = imgCloth.height * clothScale;
 
-  text('Away from the too rough fingers', width / 2, 600);  // Top text
-  text('Of the            .', width / 2, height - 90);  // Bottom text
+  let clothX = width / 2 - clothW / 2;
+  let clothY = height / 2 - clothH / 2;
+
+  image(imgCloth, clothX, clothY, clothW, clothH);
+
+  let handScale = 0.4;
+  let handW = imgHand1.width * handScale;
+  let handH = imgHand1.height * handScale;
+
+  let hoverY = clothY - handH / 1.5;
+  let restY = height;
+
+  if (
+    mouseX > clothX &&
+    mouseX < clothX + clothW &&
+    mouseY > clothY &&
+    mouseY < clothY + clothH
+  ) {
+    handYTarget = hoverY;
+  } else {
+    handYTarget = restY;
+  }
+
+  handYStart = lerp(handYStart, handYTarget, handSpeed);
+
+  image(imgHand1, width / 2 - handW * 1.2, handYStart, handW, handH);
+  image(imgHand2, width / 2 + handW * 0.2, handYStart, handW, handH);
+
+  textAlign(CENTER);
+  text('Away from the too rough fingers', width / 2, 600);
+
+  textAlign(LEFT);
+  let sentenceY = height - 90;
+  let sentenceStartX = width / 2 - textWidth("Of the world.") / 2;
+
+  text("Of the", sentenceStartX, sentenceY);
+
+  let linkX = sentenceStartX + textWidth("Of the ");
+  let linkY = sentenceY - 47;
+  link.position(linkX, linkY);
+
+  text(".", linkX + textWidth("world "), sentenceY);
 }
